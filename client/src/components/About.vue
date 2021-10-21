4345c28d82304cf2212c4daafc3c75625723c60d
yangyezhuang <365939114@qq.com>
2021-10-21 19:47:56 +0800

    update

--- client/src/components/About.vue
+++ client/src/components/About.vue
@@ -42,14 +42,14 @@
           <li class="blog-wrapper">
             <h2 class="blog-title">技能：</h2>
             <p>
-              1. 熟悉 Hadoop 大数据平台的搭建与配置，日常维护与优化。<br />
-              2. 掌握 Python 语言及面向对象的设计思想。<br />
-              3. 擅长 Python 爬虫以及数据分析与可视化。<br />
-              4. 熟悉 Web Service 技术，前端方面了解 html，css，JavaScript。<br />
-              5. 能够基于 Node.js 与 Vue 框架进行开发。<br />
-              6. 熟悉使用 Linux 系统，用来搭建环境以及部署工作。<br />
-              7. 熟悉 MySQL，MongoDB 的操作。<br />
-              8. 熟悉面向对象开发，理解函数式编程，有良好编码习惯。
+              1. 熟悉 Hadoop 大数据平台的搭建与配置，日常维护与优化<br />
+              2. 掌握 Python 语言及面向对象的设计思想<br />
+              3. 擅长 Python 爬虫以及数据分析与可视化<br />
+              4. 熟悉 Web Service 技术，前端方面了解 html，css，JavaScript<br />
+              5. 能够基于 Node.js 与 Vue 框架进行开发<br />
+              6. 熟悉使用 Linux 系统，用来搭建环境以及部署工作<br />
+              7. 熟悉 MySQL，MongoDB 的操作<br />
+              8. 熟悉面向对象开发，理解函数式编程，有良好编码习惯
             </p>
             <br />
           </li>
@@ -65,18 +65,18 @@
               2021 年 5 月 二手房数据分析
               (https://github.com/yangyezhuang/Python)<br />
               <b>项目描述：</b><br />
-              &emsp;&emsp;对五个一线城市的二手房的价格以及二手房购置指标解析分析。
-              并将分析结果实现可视化展示。<br />
+              &emsp;&emsp;对五个一线城市的二手房的价格以及二手房购置指标解析分析
+              并将分析结果实现可视化展示<br />
               <b>技术描述：</b><br />
               &emsp;&emsp;使用技术：Python，pandas，numpy，pyecharts<br />
               <b>开发描述：</b><br />
               &emsp;&emsp;1. 使用 Python
-              异步方法处理网络请求，提高效率，加快爬取速度。<br />
+              异步方法处理网络请求，提高效率，加快爬取速度<br />
               &emsp;&emsp;2. 通过 pandas 与 numpy
-              对数据进行清洗，删除脏数据。<br />
+              对数据进行清洗，删除脏数据<br />
               &emsp;&emsp;3. 让 pyecharts
-              获取结果，绑定数据，实现数据可视化。<br />
-              &emsp;&emsp;4. 生成动态 html 文件，并部署到托管主机上。
+              获取结果，绑定数据，实现数据可视化<br />
+              &emsp;&emsp;4. 生成动态 html 文件，并部署到托管主机上
             </p>
             <br />
           </li>
